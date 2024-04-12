5458ea7455835a514e8e79227e4a55e98a826483
huijeong365 <sarahcho87@gmail.com>
2024-04-12 10:32:39 +0900

    BoardLab

--- BoardLab/src/biz/board/BoardDAO.java
+++ BoardLab/src/biz/board/BoardDAO.java
@@ -18,6 +18,12 @@ public class BoardDAO {
 			"insert into board (seq, title, writer, content) values " + "((select nvl(max(seq),0) + 1, from board), ?, ?, ?)"; 
 	private static String BOARD_LIST =
 			"select * from board";
+	private static String BOARD_UPDATE =
+			"update board set title=?, content=? where seq=?";
+	private static String BOARD_DELETE =
+			"delete board where seq=?";
+	private static String BOARD_GET =
+			"select * from board where seq=?";
 	
 	public void insertBoard (BoardVO vo) {
 		try {
@@ -56,4 +62,58 @@ public class BoardDAO {
 		}
 		return boardList;
 	}
+	
+	public void updateBoard (BoardVO vo) {
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_UPDATE);
+			stmt.setString(1, vo.getTitle());
+			stmt.setString(2, vo.getContent());
+			stmt.setInt(3, vo.getSeq());
+			stmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(stmt, conn);
+		}
+	}
+	
+	public void deleteBoard (BoardVO vo) {
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_DELETE);
+			stmt.setInt(1, vo.getSeq());
+			stmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(stmt, conn);
+		}
+	}
+	
+	public BoardVO getBoard(BoardVO vo) {
+		//리턴 시키고자 하는 객체 준비 : DB에서 셀렉트 된 데이터가 들어갈 객체
+		BoardVO board = null;
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_GET);
+			stmt.setInt(1, vo.getSeq());
+			rs = stmt.executeQuery();
+			if(rs.next()) {
+				board = new BoardVO();
+				board.setSeq(rs.getInt("SEQ"));
+				board.setTitle(rs.getString("TITLE"));
+				board.setWriter(rs.getString("WRITER"));
+				board.setContent(rs.getString("CONTENT"));
+				board.setRegDate(rs.getDate("REGDATE"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(rs, stmt, conn);
+		}
+		return board;
+	}
+	
+	
 }
